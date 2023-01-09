852ac8831b590df19d1a56dec6c9e703fc0a78bf
DenScobar <gdv201099@gmail.com>
2023-01-09 12:01:53 +0300

    add MongoDB and UserSchema

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://ScobarDen:JeYnmmnWsLF6f5M8@cluster0.d3npoq1.mongodb.net/?retryWrites=true&w=majority')
+    .then(()=>{
+        console.log('DB ok');
+    })
+    .catch((err)=>{
+        console.log('DB error', err);
+    });
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, world man');
+})
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'John Smith'
+    }, 'secret123');
+    res.json({
+        success: true,
+        token
+    });
 })
 
 app.listen(4444, (err) => {
