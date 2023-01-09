9f714ad3bd986002e6fa161dc60459aa043535bd
DenScobar <gdv201099@gmail.com>
2023-01-09 12:45:28 +0300

    add bcrypt for password and saving userDoc

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 import { registerValidation } from "./validations/auth.js";
 import { validationResult } from "express-validator";
+
+import UserModel from "./models/User.js";
 
 mongoose
   .connect(
@@ -20,11 +23,25 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
+app.post("/auth/register", registerValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
+
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    passwordHash,
+    avatarUrl: req.body.avatarUrl,
+  });
+
+  const user = await doc.save();
+
   res.json({
     success: true,
   });
