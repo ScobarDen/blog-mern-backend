2b557d715626cf6179e1ae7635a05b064ea30b1b
DenScobar <gdv201099@gmail.com>
2023-01-09 12:33:04 +0300

    update register validation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
 import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
 
 mongoose
   .connect(
@@ -19,7 +20,15 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", (req, res) => {});
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+  res.json({
+    success: true,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
