6987808d54349eeb8983ebe596a9948308e0d801
ScobarDen <gdv-ps@mail.ru>
2023-01-10 15:45:50 +0300

    create uploading images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import multer from "multer";
 import {
   loginValidations,
   postCreateValidation,
@@ -22,16 +23,34 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
+
 app.post("/auth/login", loginValidations, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  })
+});
 
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
 app.delete("/posts/:id", checkAuth, PostController.remove);
-app.patch("/posts/:id",checkAuth, PostController.update);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
