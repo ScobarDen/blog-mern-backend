303f4104439e80db4861628c737a51a78ab52cbf
ScobarDen <gdv-ps@mail.ru>
2023-01-10 15:26:09 +0300

    add other controllers

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
-import {loginValidations, postCreateValidation, registerValidation} from "./validations.js";
+import {
+  loginValidations,
+  postCreateValidation,
+  registerValidation,
+} from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
 import * as PostController from "./controllers/PostController.js";
-
 
 mongoose
   .connect(
@@ -25,10 +28,10 @@ app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.get("/posts", PostController.getAll);
-// app.get("/posts/:id", PostController.getOne);
-app.post("/posts",checkAuth, postCreateValidation, PostController.create);
-// app.delete("/posts", PostController.remove);
-// app.patch("/posts", PostController.update);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id",checkAuth, PostController.update);
 
 app.listen(4444, (err) => {
   if (err) {
