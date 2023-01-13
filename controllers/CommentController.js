bb9576e664af5f6fd9e912611baf7e1ba7147efd
ScobarDen <gdv-ps@mail.ru>
2023-01-13 15:31:34 +0300

    add comment to Post

---
+++ controllers/CommentController.js
@@ -0,0 +1,140 @@
+import CommentModel from "../models/Comment";
+
+export const create = async (req, res) => {
+  try {
+    const doc = new CommentModel({
+      text: req.body.text,
+      user: req.userId,
+    });
+    const post = await doc.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось создать комментарий",
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const comments = await CommentModel.find().populate("user").exec();
+    res.json(comments);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьи",
+    });
+  }
+};
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = [...new Set(posts.map((post) => post.tags).flat())].slice(
+      0,
+      5
+    );
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить тэги",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndUpdate(
+      { _id: postId },
+      { $inc: { viewsCount: 1 } },
+      { returnDocument: "after" },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Не удалось получить статью",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json(doc);
+      }
+    ).populate("user");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить статьюююю",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndDelete({ _id: postId }, (err, doc) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          message: "Не удалось удалить статью",
+        });
+      }
+      if (!doc) {
+        return res.status(404).json({
+          message: "Статья не найдена",
+        });
+      }
+      res.json({
+        success: true,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось удалить статьюююю",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.updateOne(
+      { _id: postId },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Не удалось обновить статью",
+          });
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: "Статья не найдена",
+          });
+        }
+        res.json({
+          success: true,
+        });
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось обновить статью",
+    });
+  }
+};
