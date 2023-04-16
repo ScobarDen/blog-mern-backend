db324e7dc2979ad7837e9ef1d7ca52adcfdf754e
ScobarDen <gdv-ps@yandex.ru>
2023-04-16 15:49:14 +0300

    refactor routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import authRoutes from "./auth.routes.js";
+import commentsRoutes from "./comments.routes.js";
+import postsRoutes from "./posts.routes.js";
+import tagsRoutes from "./tags.routes.js";
+import uploadRoutes from "./upload.routes.js";
 const router = express.Router({ mergeParams: true });
 
-// /api/auth
-// router.use("/auth", require("./auth.routes"));
-// router.use("/comment", require("./comment.routes"));
-// router.use("/profession", require("./profession.routes"));
-// router.use("/quality", require("./quality.routes"));
-// router.use("/user", require("./user.routes"));
+router.use("/auth", authRoutes);
+router.use("/comments", commentsRoutes);
+router.use("/tags", tagsRoutes);
+router.use("/posts", postsRoutes);
+router.use("/upload", uploadRoutes);
 
 export default router;
